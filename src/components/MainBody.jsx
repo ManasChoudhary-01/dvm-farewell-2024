00f13ac646097780292be8979c34b0e000916ab9
Manas <manasck14@gmail.com>
2024-11-21 18:53:26 +0530

    feat: added button functionlaity

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import "./MainBody.css";
 import SideMenu from "./SideMenu";
 import Carousel from "./Carousel";
@@ -9,14 +11,32 @@ const data = [
     vertical: "front",
     img: "../",
   },
+  {
+    name: "manas choudhary",
+    vertical: "front",
+    img: "../",
+  },
 ];
 
 export default function MainBody() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1
+    );
+  };
   return (
     <div className="mainbody">
       <SideMenu></SideMenu>
-      <PlaylistSection></PlaylistSection>
-      <Carousel items={data}></Carousel>
+      <PlaylistSection prev={handlePrev} next={handleNext} />
+      <Carousel items={[data[currentIndex]]} ></Carousel>
     </div>
   );
 }
