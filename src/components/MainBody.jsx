8d59737cb0cc64b22522080d117e1888c31d1535
Manas <manasck14@gmail.com>
2024-11-21 18:47:27 +0530

    carousel data

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,14 +1,22 @@
-import "./MainBody.css"
-import SideMenu from "./SideMenu"
-import Carousel from "./Carousel"
-import PlaylistSection from "./PlaylistSection"
+import "./MainBody.css";
+import SideMenu from "./SideMenu";
+import Carousel from "./Carousel";
+import PlaylistSection from "./PlaylistSection";
 
-export default function MainBody(){
-    return (
-        <div className="mainbody">
-            <SideMenu></SideMenu>
-            <PlaylistSection></PlaylistSection>
-            <Carousel></Carousel>
-        </div>
-    )
+const data = [
+  {
+    name: "manas",
+    vertical: "front",
+    img: "../",
+  },
+];
+
+export default function MainBody() {
+  return (
+    <div className="mainbody">
+      <SideMenu></SideMenu>
+      <PlaylistSection></PlaylistSection>
+      <Carousel items={data}></Carousel>
+    </div>
+  );
 }
